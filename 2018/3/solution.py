f2630ee657f4f3d15cd923b2102bd51b4b460108
Tienlong Pham <tpham@seismic.com>
2019-01-15 00:33:46 -0600

    Use set arithmetic to accomplish part 2 of day 3

--- 2018/3/solution.py
+++ 2018/3/solution.py
@@ -6,18 +6,27 @@ def lines():
     for line in fileinput.input():
         yield line.strip()
 
-def part1():
+def claims_dict():
     claims = defaultdict(set)
     for line in lines():
         claim, x, y, w, h = map(int, re.findall(r'\d+', line))
         for i in range(x, x + w):
             for j in range(y, y + h):
                 claims[(i, j)].add(claim)
-    return sum(len(value) > 1 for value in claims.values())
+    return claims
 
+def part1():
+    return sum(len(value) > 1 for value in claims_dict().values())
+
+def part2():
+    values = claims_dict().values()
+    singles = set.union(*[value for value in values if len(value) is 1])
+    overlaps = set.union(*[value for value in values if len(value) > 1])
+    return (singles - overlaps).pop()
 
 def main():
     print(part1())
+    print(part2())
 
 if __name__ == '__main__':
     main()
