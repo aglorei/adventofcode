19a600cbf934cc47083bfcc798e0d7dc71da1e6f
Tienlong Pham <tpham@seismic.com>
2019-01-15 21:30:03 -0600

    Init SleepSchedule OOP solution to day 4

--- 2018/4/solution.py
+++ 2018/4/solution.py
@@ -3,55 +3,59 @@ from datetime import datetime
 import fileinput
 import re
 
-def lines():
-    for line in sorted(fileinput.input(), key=parse_timestamp):
-        yield line.strip()
+class SleepSchedule:
+    def __init__(self):
+        self.totals = defaultdict(int)
+        self.minutes = defaultdict(lambda: defaultdict(int))
+        for line in self.lines():
+            if 'Guard' in line:
+                guard = self.parse_guard(line)
+            elif 'falls asleep' in line:
+                start = self.parse_minute(line)
+            elif 'wakes up' in line:
+                for minute in range(start, self.parse_minute(line)):
+                    self.totals[guard] += 1
+                    self.minutes[guard][minute] += 1
 
-def parse_guard(line):
-    return int(re.findall(r'\d+', line[19:])[0])
+    def lines(self):
+        for line in sorted(fileinput.input(), key=self.parse_timestamp):
+            yield line.strip()
 
-def parse_minute(line):
-    return int(line[15:17])
+    def part1(self):
+        best = max(self.totals, key=self.totals.get)
+        minute = max(self.minutes[best], key=self.minutes[best].get)
+        return best * minute
 
-def parse_timestamp(line):
-    pattern = '%Y-%m-%d %H:%M'
-    return datetime.strptime(line[1:17], pattern)
+    def part2(self):
+        best = max(self.max_counts, key=self.max_counts.get)
+        minute = self.max_minutes[best]
+        return best * minute
 
-def part1():
-    totals = defaultdict(int)
-    minutes = defaultdict(lambda: defaultdict(int))
-    for line in lines():
-        if 'Guard' in line:
-            guard = parse_guard(line)
-        elif 'falls asleep' in line:
-            start = parse_minute(line)
-        elif 'wakes up' in line:
-            for minute in range(start, parse_minute(line)):
-                totals[guard] += 1
-                minutes[guard][minute] += 1
-    best = max(totals, key=totals.get)
-    minute = max(minutes[best], key=minutes[best].get)
-    return best * minute
+    @property
+    def max_counts(self):
+        return {k: v[max(v, key=v.get)] for k, v in self.minutes.items()}
 
-def part2():
-    minutes = defaultdict(lambda: defaultdict(int))
-    for line in lines():
-        if 'Guard' in line:
-            guard = parse_guard(line)
-        elif 'falls asleep' in line:
-            start = parse_minute(line)
-        elif 'wakes up' in line:
-            for minute in range(start, parse_minute(line)):
-                minutes[guard][minute] += 1
-    max_counts = {k: v[max(v, key=v.get)] for k, v in minutes.items()}
-    max_minutes = {k: max(v, key=v.get) for k, v in minutes.items()}
-    best = max(max_counts, key=max_counts.get)
-    minute = max_minutes[best]
-    return best * minute
+    @property
+    def max_minutes(self):
+        return {k: max(v, key=v.get) for k, v in self.minutes.items()}
+
+    @staticmethod
+    def parse_guard(line):
+        return int(re.findall(r'\d+', line[19:])[0])
+
+    @staticmethod
+    def parse_minute(line):
+        return int(line[15:17])
+
+    @staticmethod
+    def parse_timestamp(line):
+        pattern = '%Y-%m-%d %H:%M'
+        return datetime.strptime(line[1:17], pattern)
 
 def main():
-    print(part1())
-    print(part2())
+    sleep_schedule = SleepSchedule()
+    print(sleep_schedule.part1())
+    print(sleep_schedule.part2())
 
 if __name__ == '__main__':
     main()
