e9ab6440fe513892074ed8e38134336b07932813
Tienlong Pham <tpham@seismic.com>
2019-01-20 23:35:37 -0600

    Init part 2 solution to day 6

--- 2018/6/solution.py
+++ 2018/6/solution.py
@@ -10,7 +10,7 @@ def parse_coordinate(line):
 def distance(a, b):
     return sum(abs(x-y) for x, y in zip(a, b))
 
-def distance_collection(origin, coordinates):
+def sorted_distances(origin, coordinates):
     enum = enumerate(coordinates)
     return sorted((distance(origin, (cx, cy)), (cx, cy)) for i, (cx, cy) in enum)
 
@@ -22,7 +22,7 @@ def part1():
     y_min, y_max = min(y for x, y in coordinates), max(y for x, y in coordinates)
     for x in range(x_min, x_max+1):
         for y in range(y_min, y_max+1):
-            distances = distance_collection((x, y), coordinates)
+            distances = sorted_distances((x, y), coordinates)
             if distances[0][0] != distances[1][0]:
                 counts[distances[0][1]] += 1
                 if x == x_min or x == x_max or y == y_min or y == y_max:
@@ -31,8 +31,21 @@ def part1():
         counts.pop(coordinate)
     return max(counts.values())
 
+def part2(threshold=10000):
+    coordinates = lines()
+    count = 0
+    x_min, x_max = min(x for x, y in coordinates), max(x for x, y in coordinates)
+    y_min, y_max = min(y for x, y in coordinates), max(y for x, y in coordinates)
+    for x in range(x_min, x_max+1):
+        for y in range(y_min, y_max+1):
+            distances = sorted_distances((x, y), coordinates)
+            if sum(d for d, c in distances) < threshold:
+                count += 1
+    return count
+
 def main():
     print(part1())
+    print(part2())
 
 if __name__ == '__main__':
     main()
